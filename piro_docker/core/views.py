4e8b0831b6822280abe90b643d4a2a4922ef6239
happysmileboy <t35114105@gmail.com>
2022-02-19 00:16:06 +0900

    정말 간단한 post

--- piro_docker/core/views.py
+++ piro_docker/core/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-def hello_world(request: HttpRequest):
+from core.models import Post
 
-    return HttpResponse('Hello World!')
+
+def hello_world(request: HttpRequest):
+    post, _ = Post.objects.get_or_create(title='hi')
+    post.count += 1
+    post.save()
+    count = post.count
+    need_count = 10 - count
+    if need_count > 0:
+        return HttpResponse(f'Hello World! Your count is {post.count}. plz enter {need_count} more!')
+    else:
+        post.count = 0
+        post.save()
+        return HttpResponse(f'Hello World! Your count is {count}. Thank you')
