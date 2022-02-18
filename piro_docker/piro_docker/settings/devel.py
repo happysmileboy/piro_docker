cb4163a9b1b4087630bc869a790ae6c4540226f5
happysmileboy <t35114105@gmail.com>
2022-02-19 02:39:40 +0900

    s3 및 cloudfront 설정

--- piro_docker/piro_docker/settings/devel.py
+++ piro_docker/piro_docker/settings/devel.py
@@ -11,3 +11,22 @@ DATABASES = {
         'PORT': os.environ['DATABASE_PORT'],
     }
 }
+
+# AWS MEDIA
+AWS_REGION = os.environ['AWS_REGION']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_HOST = 's3.%s.amazonaws.com' % AWS_REGION
+AWS_S3_CUSTOM_DOMAIN = os.environ['AWS_CLOUDFRONT_DOMAIN']
+
+# Static Setting
+AWS_STATIC_LOCATION = 'static'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Media Setting
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
--- piro_docker/piro_docker/settings/local.py
+++ piro_docker/piro_docker/settings/local.py
@@ -12,3 +12,22 @@ DATABASES = {
     }
 }
 
+# AWS MEDIA
+AWS_REGION = get_secrets('AWS_REGION')
+AWS_STORAGE_BUCKET_NAME = get_secrets('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = get_secrets('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = get_secrets('AWS_SECRET_ACCESS_KEY')
+
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_HOST = 's3.%s.amazonaws.com' % AWS_REGION
+AWS_S3_CUSTOM_DOMAIN = get_secrets('AWS_CLOUDFRONT_DOMAIN')
+
+# Static Setting
+AWS_STATIC_LOCATION = 'static'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Media Setting
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
